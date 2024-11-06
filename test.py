e3bb7a37e37ebc123e861a1926b1e892345b53a6
yarikopython <kozoderovaroslav@gmail.com>
2024-11-06 19:05:52 +0100

    Added Example

--- test.py
+++ test.py
@@ -1,6 +1,32 @@
-import keyboard
-from py.gui import detect
+import pyrobloxbot
 
-keyboard.add_hotkey("F1", lambda: detect())
+"""example for Collection"""
 
-keyboard.wait("F3")
+class Collection:
+
+    def nonvipspot1():
+        pass
+
+    def nonvipspot2():
+        pass
+
+    def nonvipspot3():
+        pass
+
+    def nonvipspot4():
+        pass
+
+    def nonvipspot5():
+        pass
+    
+    def nonvipspot6():
+        pass
+
+    def nonvipspots():
+        Collection.nonvipspot1()
+        Collection.nonvipspot2()
+        Collection.nonvipspot3()
+        Collection.nonvipspot4()
+        Collection.nonvipspot5()
+        Collection.nonvipspot6()
+        
